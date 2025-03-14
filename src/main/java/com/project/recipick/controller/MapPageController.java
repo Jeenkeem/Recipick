91a98b767059d61fe1e22b8ad1a0b8ccb146c812
jihoon <wlgns120100@naver.com>
2025-03-14 13:09:25 +0900

    Hide Kakao API Key

--- src/main/java/com/project/recipick/controller/MapPageController.java
+++ src/main/java/com/project/recipick/controller/MapPageController.java
@@ -1,14 +1,22 @@
 package com.project.recipick.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
 public class MapPageController {
     
+    @Value("${kakao.api.key}")
+    private String kakaoApiKey;
+
     @GetMapping("mapPage")
-    public String getMethodName() {
+    public String getMethodName(Model model) {
+
+        model.addAttribute("kakaoApiKey", kakaoApiKey);
+
         return "map/mapPage";
     }
     
