e4809902e7022777b39101c1b7d94de93c8b025f
JH1201 <wlgns120100@naver.com>
2025-03-25 22:53:42 +0900

    데이터 저장 RestController 생성

--- src/main/java/com/project/recipick/controller/IndexController.java
+++ src/main/java/com/project/recipick/controller/IndexController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -21,12 +22,31 @@ public class IndexController {
 
     @GetMapping("/recipick/main")
     public String getMethodName() {
-        List<RecipeInfo> recipeInfoList = recipeOrderService.recipieFindAll();
+        List<RecipeInfo> recipeInfoList = recipeOrderService.findAllRecipe();
 
         for(int i=0; i<recipeInfoList.size(); i++) {
-            System.out.println(recipeInfoList.get(i));
+            System.out.println(recipeInfoList.get(i).getRECIPE_NM_KO());
         }
 
+        RecipeInfo re = new RecipeInfo();
+        re.setRECIPE_ID(1);
+        re.setRECIPE_NM_KO("나물");
+        re.setSUMRY("Hello world");
+        re.setNATION_CODE("AA");
+        re.setNATION_NM("Ko");
+        re.setTY_CODE("23");
+        re.setTY_NM("sample");
+        re.setCOOKING_TIME("30분");
+        re.setCALORIE("300");
+        re.setQNT("dhjke");
+        re.setLEVEL_NM("LOW");
+        re.setIRDNT_CODE("jelo");
+        re.setPC_NM("helo");
+
+        List<RecipeInfo> list = new ArrayList<>();
+        list.add(re);
+
+        recipeOrderService.saveRecipe(list);
 
         return "mainPage/main";
     }
--- src/main/java/com/project/recipick/mapper/RecipeInfoMapper.java
+++ src/main/java/com/project/recipick/mapper/RecipeInfoMapper.java
@@ -9,4 +9,6 @@ import java.util.List;
 public interface RecipeInfoMapper {
 
     List<RecipeInfo> findAll();
+
+    void saveRecipe(RecipeInfo re);
 }
