0050d1bcb698881732be1afa0cdce758cbd534d0
jihoon <wlgns120100@naver.com>
2025-04-02 16:18:43 +0900

    카카오맵 키워드 검색

--- src/main/java/com/project/recipick/controller/DataController.java
+++ src/main/java/com/project/recipick/controller/DataController.java
@@ -5,17 +5,21 @@ import com.project.recipick.Entity.RecipeInfo;
 import com.project.recipick.service.MartInfoService;
 import com.project.recipick.service.RecipeOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/recipick")
-public class DataSaveController {
+public class DataController {
 
     @Autowired
     final RecipeOrderService recipeOrderService;
@@ -23,11 +27,17 @@ public class DataSaveController {
     @Autowired
     final MartInfoService martInfoService;
 
-    public DataSaveController(RecipeOrderService recipeOrderService, MartInfoService martInfoService) {
+    public DataController(RecipeOrderService recipeOrderService, MartInfoService martInfoService) {
         this.recipeOrderService = recipeOrderService;
         this.martInfoService = martInfoService;
     }
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+
+    @Value("${kakao.restapi.key}")
+    private String kakaoRestApiKey;
 
     @GetMapping("/saveRecipe")
     public void SaveRecipe() {
@@ -63,5 +73,25 @@ public class DataSaveController {
     public List<MartInfo> getMartInfo(@RequestParam String M_NAME) {
         return martInfoService.findMartInfo(M_NAME);
     }
+
+    @GetMapping("/findMartLocation")
+    public ResponseEntity<String> findMartLocation() {
+        // API 키 설정
+        String apiKey = "KakaoAK "  + kakaoRestApiKey;
+        String url = "https://dapi.kakao.com/v2/local/search/keyword.json"
+                + "?y=37.514322572335935&x=127.06283102249932&radius=20000&query=카카오프렌즈";
+
+        // 헤더 설정
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", apiKey);
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        // API 요청 및 응답
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+
+        // 응답 반환
+        return response;
+    }
+
 
 }
