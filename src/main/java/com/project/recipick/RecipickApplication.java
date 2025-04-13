3be870f18d809c49b976ba8a3516648ff7853f10
JH1201 <wlgns120100@naver.com>
2025-04-13 22:08:58 +0900

    자치구별 시장 마커출력

--- src/main/java/com/project/recipick/RecipickApplication.java
+++ src/main/java/com/project/recipick/RecipickApplication.java
@@ -10,8 +10,7 @@ public class RecipickApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecipickApplication.class, args);
-    
-		System.out.println("push push");
+
 	}
 
 }
