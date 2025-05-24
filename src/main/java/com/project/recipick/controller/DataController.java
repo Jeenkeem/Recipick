502324c5469bcb3dfdf4a1734163bcf2d967cf17
iwannabeit <140404545+iwannabeit@users.noreply.github.com>
2025-05-24 18:24:39 +0900

    [250524|최선빈] ✅recipeRecommend Page 기능 구현 완료 (css 미적용)

--- src/main/java/com/project/recipick/controller/DataController.java
+++ src/main/java/com/project/recipick/controller/DataController.java
@@ -161,4 +161,11 @@ public class DataController {
         recipeCardService.callRecipeApiAndSaveData(apiKey, addr);
     }
 
+    @GetMapping("/saveRecipeIngredient")
+    public void callRecipeIngredientApi(){
+        String apiKey = "2a2d98088a90a23a81db461c5bd31675ca4cb35b994183c8b27182fe01fd45f8";
+        String addr = "http://211.237.50.150:7080/openapi/";
+        recipeCardService.callRecipeIngredientAndSaveData(apiKey, addr);
+    }
+
 }
--- src/main/java/com/project/recipick/mapper/RecipeCardMapper.java
+++ src/main/java/com/project/recipick/mapper/RecipeCardMapper.java
@@ -1,12 +1,23 @@
 package com.project.recipick.mapper;
 
 import com.project.recipick.Entity.RecipeCardEntity;
+import com.project.recipick.Entity.RecipeIngredientEntity;
 import com.project.recipick.dto.RecipeCardDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
+import java.util.Map;
+
 @Mapper
 public interface RecipeCardMapper {
     void insertMAFRARecipes(List<RecipeCardEntity> recipeCardEntities);
+
+    void insertIngredient(List<RecipeIngredientEntity> recipeIngredientEntities);
     List<RecipeCardDTO> getRecipeCardList();
+
+    List<RecipeCardDTO> getSearchedRecipeList(String searchWord);
+
+    List<RecipeIngredientEntity> getSearchedIngredientList(String inputIngredient);
+
+    List<RecipeCardDTO> getRecommendRecipe(Map<String, Object> paramMap);
 }
