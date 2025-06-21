6d1248a29a012b2df81ee538f49e0dfb7e73423d
JH1201 <wlgns120100@naver.com>
2025-06-21 12:29:39 +0900

    홈 페이지에 지도로 이동 추가

--- src/main/java/com/project/recipick/controller/MainPageController.java
+++ src/main/java/com/project/recipick/controller/MainPageController.java
@@ -1,13 +1,21 @@
 package com.project.recipick.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainPageController {
 
+    @Value("${kakao.api.key}")
+    private String kakaoApiKey;
+
     @GetMapping("/recipick")
-    public String home(){
+    public String home(Model model){
+
+        model.addAttribute("kakaoApiKey", kakaoApiKey);
+
         return "MainPage";
     }
 }
