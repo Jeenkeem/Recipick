5e413bb8e93c5e3f4fd4f5cb6db2970d3953aa6a
Jihoon Kim <96639809+JH1201@users.noreply.github.com>
2025-07-02 17:52:11 +0900

    Update RecipickApplication.java

--- src/main/java/com/project/recipick/RecipickApplication.java
+++ src/main/java/com/project/recipick/RecipickApplication.java
@@ -11,6 +11,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class RecipickApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RecipickApplication.class, args);
-		// test pr
+		// test pr 2
 	}
 }
