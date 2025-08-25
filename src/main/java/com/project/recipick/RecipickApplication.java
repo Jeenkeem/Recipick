9c93cbbbaf34b78a095a7ee70501cc52e6961584
Jeenkeem <146731683+Jeenkeem@users.noreply.github.com>
2025-08-25 18:09:31 +0900

    Update RecipickApplication.java
    충돌 내용 제거

--- src/main/java/com/project/recipick/RecipickApplication.java
+++ src/main/java/com/project/recipick/RecipickApplication.java
@@ -5,11 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-<<<<<<< HEAD
 @MapperScan("com.project.recipick.mapper")
 @EnableScheduling
-=======
->>>>>>> 4bde1dac5f6c1063e700682a5ea937fbe7b65427
 public class RecipickApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RecipickApplication.class, args);
